6681c08fb4afd81bd1ed20b36f9261b7014a08bb
Tharunreddy <tharunreddy.csit@gmail.com>
2025-04-30 15:18:46 -0400

    assignment 1

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+# import pandas as pd
+
+# dataset = pd.read_csv("archive/walmart.csv")
+
+# print(dataset.describe())
+
+# columns = ["Weekly_Sales", "Holiday_Flag", "Temperature", "Fuel_Price", "CPI", "Unemployment"]
+
+# for column in columns:
+#     print("Mean: ", dataset[column].mean())
+#     print("Median: ", dataset[column].median())
+#     print("Mode: ", dataset[column].mode())
 import pandas as pd
 
-dataset = pd.read_csv("archive/walmart.csv")
+dataset = pd.read_csv("archive/Walmart.csv")
+print("Dataset loaded")
 
-print(dataset.describe())
+print(dataset.head())
 
-columns = ["Weekly_Sales", "Holiday_Flag", "Temperature", "Fuel_Price", "CPI", "Unemployment"]
+dataset["DiscountApplied"] = False
+dataset.loc[dataset["Weekly_Sales"] > 1000, "DiscountApplied"] = True
+dataset.loc[dataset["Weekly_Sales"] > 1000, "Weekly_Sales"] *= 0.9
 
-for column in columns:
-    print("Mean: ", dataset[column].mean())
-    print("Median: ", dataset[column].median())
-    print("Mode: ", dataset[column].mode())
+dataset.to_csv("New_Walmart.csv", index=False)
+print("Changes saved to New_Walmart.csv")
